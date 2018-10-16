49d7b45286c9fe9fc751004aa80a4f8bcd38357d
anand31 <imanand31@gmail.com>
2018-10-16 15:56:21 +0530

    typo correct

--- src/ttt.java
+++ src/ttt.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class ttt {
-	
+
 	//private static char a[][];
 	public static void main(String[]args)
 	{
 		String a[][]= new String[3][3];
-		
-		
+
+
 		Scanner scan= new Scanner(System.in);
 		Random rand= new Random();
-		
+
 		System.out.println("");
 		System.out.println("");
 		System.out.println("\t\t\t\t!!! THIS IS A TIC-TAC-TOE GAME !!! ");
@@ -34,16 +34,16 @@ public class ttt {
 		System.out.println("");
 		System.out.println("");
 		System.out.println("");
-	//**********************************************************************************************************************************************************************************	
+	//**********************************************************************************************************************************************************************************
 		System.out.println("COMPUTER's turn");
 		System.out.println("");
 		int row=rand.nextInt(3);
 		int col=rand.nextInt(3);
-		
+
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
-				
-				
+
+
 				if(i==row && j==col){
 					a[i][j]="O";
 				}
@@ -51,24 +51,24 @@ public class ttt {
 					{
 					a[i][j]=" ";
 					}
-				
+
 				System.out.print(" |"+a[i][j]+" ");
-				
-				
+
+
 			}
-			
+
 			System.out.println("");
 		}
-		
+
 		System.out.println("");
-		
-//*************************************************************************************************************************************************************************************		
-		
+
+//*************************************************************************************************************************************************************************************
+
 		System.out.println("");
-		System.out.println("your turn");
+		System.out.println("now your turn");
 		System.out.println("");
 		System.out.println("enter the position where you wanted to enter X (row  column)");
-		
+
 		int row0= scan.nextInt();
 		int col0= scan.nextInt();
 
@@ -76,7 +76,7 @@ public class ttt {
 		System.out.println("");
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
-				
+
 				if(i==row0 && j==col0){
 					a[i][j]="X";
 				}
@@ -84,26 +84,26 @@ public class ttt {
 				else if(i==row && j==col){
 					a[i][j]="O";
 				}
-				
+
 				else
 					{
 					a[i][j]=" ";
 					}
-				
+
 				System.out.print(" |"+a[i][j]+" ");
-				
+
 			}
 			System.out.println("");
 			}
-		
+
 		System.out.println("");
-		
+
 		//******************************************************************************************************************************************************************************
-		
+
 		System.out.println("");
 		System.out.println("Computer's turn");
-		
-		
+
+
 		int arow=rand.nextInt(3);
 		int acol=rand.nextInt(3);
 		if(a[arow][acol]=="X" || a[arow][acol]=="O")
@@ -125,14 +125,14 @@ public class ttt {
 						}
 				}
 			}
-			
+
 		}
-		
+
 		System.out.println("");
 		System.out.println("");
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
-				
+
 				if(i==row0 && j==col0){
 					a[i][j]="X";
 				}
@@ -143,39 +143,39 @@ public class ttt {
 				else if(i==arow && j==acol){
 					a[i][j]="O";
 				}
-				
-				
+
+
 				else
 					{
 					a[i][j]=" ";
 					}
-				
+
 				System.out.print(" |"+a[i][j]+" ");
-				
+
 			}
 			System.out.println("");
 			}
 		System.out.println("");
-		
+
 		//***************************************************************************************************************************************************************
 
 		System.out.println("");
 		System.out.println("your turn");
 		System.out.println("");
 		System.out.println("enter the position where you wanted to enter X (row  column)");
-		
+
 		int row1= scan.nextInt();
 		int col1= scan.nextInt();
 		System.out.println("");
 		System.out.println("");
-		
+
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
-				
+
 				if(i==row0 && j==col0){
 					a[i][j]="X";
 				}
-		
+
 				else if(i==row1 && j==col1){
 					a[i][j]="X";
 				}
@@ -186,22 +186,22 @@ public class ttt {
 				else if(i==arow && j==acol){
 					a[i][j]="O";
 				}
-				
-				
+
+
 				else
 					{
 					a[i][j]=" ";
 					}
-				
+
 				System.out.print(" |"+a[i][j]+" ");
-				
+
 			}
 			System.out.println("");
 			}
 		System.out.println("");
-		
+
 		//***************************************************************************************************************************************************************
-		
+
 
 		System.out.println("");
 		System.out.println("Computer's turn");
@@ -209,12 +209,12 @@ public class ttt {
 		int bcol=rand.nextInt(3);
 		if(a[brow][bcol]=="X" || a[brow][bcol]=="O")
 		{
-			
+
 			brow=brow+1;
 				if(brow>2){
 					brow=brow-2;
 				}
-				
+
 					if(a[brow][bcol]=="X" || a[brow][bcol]=="O")
 					{
 						bcol=bcol+1;
@@ -245,20 +245,20 @@ public class ttt {
 									System.out.println("");
 								}
 							}
-							
+
 						}
 				}
-				
-			
+
+
 		}
-		
+
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
-				
+
 				if(i==row0 && j==col0){
 					a[i][j]="X";
 				}
-		
+
 				else if(i==row1 && j==col1){
 					a[i][j]="X";
 				}
@@ -272,38 +272,38 @@ public class ttt {
 				else if(i==brow && j==bcol){
 					a[i][j]="O";
 				}
-				
-				
+
+
 				else
 					{
 					a[i][j]=" ";
 					}
-				
+
 				System.out.print(" |"+a[i][j]+" ");
-				
+
 			}
 			System.out.println("");
 			}
 		System.out.println("");
-		
+
 		//*************************************************************************************************************************************************************************
-		
+
 		System.out.println("");
 		System.out.println("your turn");
 		System.out.println("");
 		System.out.println("enter the position where you wanted to enter X (row  column)");
-		
+
 		int row2= scan.nextInt();
 		int col2= scan.nextInt();
 		System.out.println("");
 		System.out.println("");
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
-				
+
 				if(i==row0 && j==col0){
 					a[i][j]="X";
 				}
-		
+
 				else if(i==row1 && j==col1){
 					a[i][j]="X";
 				}
@@ -320,37 +320,37 @@ public class ttt {
 				else if(i==brow && j==bcol){
 					a[i][j]="O";
 				}
-				
-				
+
+
 				else
 					{
 					a[i][j]=" ";
 					}
-				
+
 				System.out.print(" |"+a[i][j]+" ");
-				
+
 			}
 			System.out.println("");
 			}
 		System.out.println("");
-		
-	
-			
-		
+
+
+
+
 		//***************************************************************************************************************************************************************
 		System.out.println("");
 		System.out.println("Computer's turn");
 		int crow=rand.nextInt(3);
 		int ccol=rand.nextInt(3);
-		
+
 		if(a[crow][ccol]=="X" || a[crow][ccol]=="O")
 		{
-			
+
 			crow=crow+1;
 				if(crow>2){
 					crow=crow-2;
 				}
-				
+
 					if(a[crow][ccol]=="X" || a[crow][ccol]=="O")
 					{
 						ccol=ccol+1;
@@ -381,20 +381,20 @@ public class ttt {
 									System.out.println("");
 								}
 							}
-							
+
 						}
 				}
-				
-			
+
+
 		}
-		
+
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
-				
+
 				if(i==row0 && j==col0){
 					a[i][j]="X";
 				}
-		
+
 				else if(i==row1 && j==col1){
 					a[i][j]="X";
 				}
@@ -414,23 +414,23 @@ public class ttt {
 				else if(i==crow && j==ccol){
 					a[i][j]="O";
 				}
-				
-				
+
+
 				else
 					{
 					a[i][j]=" ";
 					}
-				
+
 				System.out.print(" |"+a[i][j]+" ");
-				
+
 			}
 			System.out.println("");
 			}
 		System.out.println("");
-		
-		
+
+
 		//***************************************************************************************************************************************************************
-		
+
 		/*int crow =0;
 		int ccol=0;
 		for(int i=0;i<3;i++){
@@ -452,7 +452,7 @@ public class ttt {
 			System.out.println("");
 		}
 		System.out.println("");
-		
+
 		//********************************************************************************************************************************************************************
 		*/
 		System.out.println("");
@@ -464,14 +464,14 @@ public class ttt {
 		int col3= scan.nextInt();
 		System.out.println("");
 		System.out.println("");
-		
+
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
-				
+
 				if(i==row0 && j==col0){
 					a[i][j]="X";
 				}
-		
+
 				else if(i==row1 && j==col1){
 					a[i][j]="X";
 				}
@@ -494,36 +494,36 @@ public class ttt {
 				else if(i==crow && j==ccol){
 					a[i][j]="O";
 				}
-				
-				
+
+
 				else
 					{
 					a[i][j]=" ";
 					}
-				
+
 				System.out.print(" |"+a[i][j]+" ");
-				
+
 			}
 			System.out.println("");
 			}
 		System.out.println("");
-		
-		
+
+
 		//***************************************************************************************************************************************************************
 
 		System.out.println("");
 		System.out.println("Computer's turn");
-		
-		
+
+
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
-				
+
 				if(a[i][j]== " " ){
 					a[i][j]= "O";
 				}
-				
+
 				System.out.print(" |"+a[i][j]+" ");
-				
+
 			}
 			System.out.println("");
 			}
@@ -542,7 +542,7 @@ public class ttt {
 			System.out.println("");
 			System.out.println("CONGRATULATIONS.....YOU WON!!!");
 		}
-		
+
 		else if(a[0][0]=="X" && a[1][0]=="X" && a[2][0]=="X" ){
 			System.out.println("");
 			System.out.println("CONGRATULATIONS.....YOU WON!!!");
@@ -563,9 +563,9 @@ public class ttt {
 			System.out.println("");
 			System.out.println("CONGRATULATIONS.....YOU WON!!!");
 		}
-		
-		
-		
+
+
+
 		else if(a[0][0]=="O" && a[0][1]=="O" && a[0][2]=="O" ){
 			System.out.println("");
 			System.out.println("HARD LUCK!!!.......COMPUTER WON");
@@ -607,13 +607,13 @@ public class ttt {
 			System.out.println("");
 			System.out.println("THE MATCH ENDS IN A DRAW!!!");
 		}
-		
-		
+
+
 		System.out.println("");
 		System.out.println("");
 		System.out.println("THANKS FOR PLAYING  \t\t developer : ADITYA SINGH");
 	}
     //thanks alot folks...this is it ...go home!!//
-	
+
 
 }
